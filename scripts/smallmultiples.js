4c054d70be5a35d9256292835ea61ab97937e989
Rémi PIERRON <remipierron@mac.home>
2025-03-30 17:18:45 +0200

    test5

--- scripts/smallmultiples.js
+++ scripts/smallmultiples.js
@@ -25,7 +25,7 @@ fetch("../data/listings.csv")
 
     // Couleurs en dégradé de rose
     const colorMap = {
-      "Entire home/apt": "##ff385d",  // rose vif
+      "Entire home/apt": "#ff385d",  // rose vif
       "Private room": "#c0ff38",      // rose moyen
       "Shared room": "#7638ff",       // rose clair
       "Hotel room": "#38ffda"         // rose pastel
