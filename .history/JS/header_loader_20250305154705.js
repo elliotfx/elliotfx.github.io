16b4e2908a3560415f1bb9706d56d8b9bd51f0d2
Elliot Feroux <elliot.feroux@etu.univ-poitiers.fr>
2025-06-10 00:38:00 +0200

    Remplacement complet par mon nouveau Portfolio

--- .history/JS/header_loader_20250305154705.js
+++
@@ -1,7 +0,0 @@
-// headerLoader.js
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Erreur lors du chargement du header :', error));
--- .history/JS/headerloader_20250305155352.js
+++
@@ -1,7 +0,0 @@
-// headerLoader.js
-fetch('html/header.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('header-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('Erreur lors du chargement du header :', error));
